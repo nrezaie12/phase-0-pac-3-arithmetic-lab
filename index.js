b39d46e425f38753d36a12e6bb6b39eedcd40bab
Neema <nrezaie12@gmail,com>
2022-04-16 14:00:05 -0400

    commit tree efadb3872fa05f4d6cce3e37181140aa5bf4cfad

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b) {
+return a+b;
+}
+function subtract(a,b) {
+return a-b;
+}
+function multiply(a,b) {
+return a*b
+}
+function divide(a,b) {
+return a/b
+}
+function add1(a){
+    return (a += 1) 
+    } 
+function increment(a){
+    return (a+1)
+}
+function decrement(a){
+    return (a-1)
+}
+function makeInt(n){
+    return parseInt(n, 10);
+} 
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+//function parseInt(`n`, 10);{
+    //return (n)
+//}
